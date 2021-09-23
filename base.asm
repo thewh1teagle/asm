761da6a93d2f8336aaa036b4f3eef2290cbbb064
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-09-23 17:15:52 +0300

    div 32 bit

--- base.asm
+++ base.asm
@@ -2,7 +2,8 @@ IDEAL
 MODEL small
 STACK 100h
 DATASEG
-procRet dw ?
+procRet dw ? ; used in functions to store function stack data
+number dd ?
 CODESEG
 proc printNewLine
     mov dl, 10
@@ -51,35 +52,43 @@ ENDP div8Bit
 ;return: S1 result, S2 remainder  
 PROC div16Bit
     pop [procRet]
-    pop ax
     pop bx
+    pop ax
     xor dx, dx
     div bx
 
-    push ax
     push dx
-
+    push ax
+    ; return result, remainder
     push [procRet]
     ret
 ENDP div16Bit
+
+; divide 32 bit number with 16 bit divider
+; params: S1: divisible, S2: divider
+PROC div32Bit
+    pop [procRet]
+
+    pop [number] ; 32 bit variable
+    pop cx ; divider
+    ; 32 bit div
+    mov ax, [word offset number + 2] ; numberL
+    mov dx, [word offset number] ; numberH
+    div cx
+    ; ax > result,  dx > remainder
+    
+    push [procRet]
+    ret
+ENDP div32Bit
 
 start:
     mov ax, @data
     mov ds, ax
     ; code here
-    
-    
-    push 20
-    push 10
-    call div8Bit
 
-    ; param in stack
-    call printDigit
-
-    push 20
     push 10
-    call div16Bit
-    call printDigit
+    push 3
+    call div32Bit
 
 exit:
     call printNewLine
